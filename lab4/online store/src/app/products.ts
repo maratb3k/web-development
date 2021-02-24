fbe92461f7412893c6a38d109f1fe24a80b0ae1f
maratb3k <60798717+maratb3k@users.noreply.github.com>
2021-02-24 10:03:11 +0600

    Add files via upload

---
+++ lab4/online store/src/app/products.ts
@@ -0,0 +1,120 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Samsung Galaxy S10Lite 128GB White',
+    price: 799,
+    description: 'Incredible prismatic colors S10 Lite attract the eye. The smartphone is available in several shades: Black, Pearl and Blue. Thanks to the smooth body lines, creating a high level of ergonomics, the S10 Lite feels comfortable in the hand.',
+    image: 'https://static.m79.lv/prod_img2/300120z7s6p4v6q2c1k1f2i6f042833z718.jpg',
+    image2: 'https://cdn.shopify.com/s/files/1/2256/3479/products/S10Lite_2048x.jpg?v=1582834942',
+    image3:'https://simsim.tj/image/cache/catalog/simsim/items/smartphones/samsung/galaxy_a/samsung_galaxy_a51_white_6-1200x800.jpg',
+    rating: 5.0,
+    link:'https://www.amazon.com/N770F-Dual-SIM-Unlocked-International-Compatible/dp/B084MD6Y5H/ref=sr_1_2?dchild=1&keywords=Samsung+Galaxy+S10+Lite+128GB+White&qid=1613994200&sr=8-2'
+    
+  },
+  {
+    id: 2,
+    name: 'Samsung Galaxy A51 64GB Black',
+    price: 699,
+    description: 'A great phone with one of the best cameras. 2 x tempered glass screen protective film, 2 x Camera Lens Screen Protectors, 2 x equipment cleaning wipe, 2 x microfiber cloth, 2 x dust stickers. If the product has a problem, please contact us first, we provide free replacement warranty and 100% satisfaction guarantee.',
+    image:'https://www.technodom.kz/media/catalog/product/d/4/d4c43c512412ccb0b36dc74135fc3095303c84cb_213585_2as.jpg',
+    image2:'https://avatars.mds.yandex.net/get-zen_doc/3770780/pub_5f3521cc5660ed7059ce95c9_5f352245c3fdab1d4bc4c76a/scale_1200',
+    image3:'https://cdn.rbt.ru/images/gen/item_image/image/7157/24/715630_r1629.jpg',
+    rating: 4.1,
+    link: 'https://www.amazon.com/Tempered-Protector-Protectors-Fingerprint-Bubble-Free/dp/B08MZWMNFZ/ref=sr_1_1_sspa?dchild=1&keywords=Samsung+Galaxy+A51+64GB+Black&qid=1613994331&s=electronics&sr=1-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExNUxZMlFBVFRVMkpRJmVuY3J5cHRlZElkPUEwNTcyNTcwMTNOUExDQlNKTDJLRyZlbmNyeXB0ZWRBZElkPUEwNTQ4MjEwMzJDRUpBSVFTTzdKRyZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU='
+  },
+  {
+    id: 3,
+    name: 'Samsung J737T Galaxy J7 Star',
+    price: 250,
+    description: 'Display diagonal, inch: 5.5. Display Resolution: 1080x2340. Operating system: Android 10.0. RAM size: 2. Built-in memory: 32. Number of SIM-cards: 2. Model Year: 2018.',
+    image:'https://techin.pk/price/assets/images/models/featured/samsung-galaxy-j7-star.jpg',
+    image2:'https://www.formatnasilatilir.net/wp-content/uploads/Samsung-Galaxy-J7-2018-format-atma.jpg',
+    image3:'https://www.unlockbase.com/blog/wp-content/samsungj7star-specs.jpg',
+    rating: 3.8,
+    link: 'https://www.amazon.com/Samsung-J737T-Galaxy-Star-T-Mobile/dp/B07FS3RNFJ/ref=sr_1_3?crid=196DDA4NVDM5S&dchild=1&keywords=samsung+j7&qid=1613997602&sprefix=samsung+j%2Caps%2C410&sr=8-3'
+  },
+  {
+    id: 4,
+    name: 'Apple iPhone 11 128GB Black',
+    price: 1059,
+    description: 'Display diagonal, inch: 5.4. Display Resolution: 1080x2340. Operating system: iOS14. RAM size: 4. Built-in memory: 32. Number of SIM-cards: 1. Model Year: 2020.',
+    image: 'https://smartshop.kz/frontend/web/image/files/product_3972923.jpg',
+    image2: 'https://maifon.ua/pub/media/catalog/product/i/p/iphone_11_b_7_3.jpg',
+    image3: 'https://itechh.kz/image/cache/catalog/iphones/11prosto/11cer2-650x650.jpg',
+    rating: 3.5,
+    link:'https://www.amazon.com/Apple-Carrier-Subscription-Cricket-Wireless/dp/B084GSPKMV/ref=sr_1_1?dchild=1&keywords=Apple+iPhone+11+128GB+Black&qid=1613994635&sr=8-1'
+  },
+  {
+    id: 5,
+    name: 'Apple iPhone 12 mini (128GB, Green)',
+    price: 678,
+    description: 'Display diagonal, inch: 5.4. Display Resolution: 1080x2340. Operating system: iOS14. RAM size: 4. Built-in memory: 128. Number of SIM-cards: 1. Model Year: 2020.',
+    image: 'https://m.media-amazon.com/images/I/71i+-WmxHWL._FMwebp__.jpg',
+    image2:'https://technosale.kz/image/cache/catalog/Smartphones/Apple/iPhone%2011/Apple%20iPhone%2011%20Green-4-1000x1000.jpg',
+    image3: 'https://itechh.kz/image/cache/catalog/iphones/11prosto/11zel4-650x650.jpg',
+    rating: 4.0,
+    link:'https://www.amazon.com/Apple-iPhone-Locked-Carrier-Subscription/dp/B08L5NWMPJ/ref=sr_1_2?dchild=1&keywords=Apple+iPhone+7&qid=1613994909&sr=8-2'
+  },
+  {
+    id: 6,
+    name: 'Apple iPhone 12 max',
+    price: 669,
+    description: 'Display diagonal, inch: 6.1. Display Resolution: 2532x1170. Operating system: iOS14. RAM size: 6. Built-in memory: 256. Number of SIM-cards: 1. Model Year: 2020.',
+    image:'https://i-cdn.phonearena.com/images/phones/78755-specs/Apple-iPhone-12-Pro-Max.jpg',
+    image2: 'https://i.ytimg.com/vi/XPSblQ4iaus/maxresdefault.jpg',
+    image3: 'https://i.pinimg.com/originals/62/7c/82/627c82ce8f2ee739d1e5af5496c9f352.jpg',
+    rating: 4.6,
+    link:'https://www.amazon.com/dp/B08L5R1CCC/ref_=vn_s_iwp_0?pd_rd_w=iNxzu&pf_rd_p=84cbcc8f-0fe2-44fc-ae2a-87f8b101dbca&pf_rd_r=WNER9H3W1SX3WX0TWDPY&pd_rd_r=3eb71853-e5c5-4432-8eda-38ce80dba27f&pd_rd_wg=Kuj4S&qid=1613997012'
+  },
+  {
+    id: 7,
+    name: 'Apple iPhone SE (64GB, Black)',
+    price: 759,
+    description: 'Display diagonal, inch: 4.7. Display Resolution: 750x1334. Operating system: iOS13. RAM size: 3. Built-in memory: 64. Number of SIM-cards: 1. Model Year: 2020.',
+    image: 'https://mobile-shop.kz/wp-content/uploads/2020/06/1-16.jpg',
+    image2:'https://msk.all-tech-shops.ru/p2249/smartfon-apple-iphone-se-128gb-white-mxd12ru-a-1.jpg',
+    image3: 'https://www.mechta.kz/export/1cbitrix/import_files/9e/9e0a18e0-86dd-11ea-a22d-005056b6dbd7.jpeg',
+    rating: 3.9,
+    link: 'https://www.amazon.com/dp/B0875RT5WS/ref=fs_a_iwp2_4#'
+  },
+  {
+    id: 8,
+    name: 'Xiaomi Redmi Note 9S',
+    price: 356,
+    description: 'Display diagonal, inch: 6.67. Display Resolution: 1080x2400. Operating system: Android 10.0. RAM size: 6. Built-in memory: 128. Number of SIM-cards: 2. Model Year: 2020.',
+    image: 'https://www.mi4canada.com/wp-content/uploads/2020/04/Aurora-Blue.jpg',
+    image2: 'https://mihome.kz/uploads/products/2570/xiaomi-redmi-note-9-pro.jpg',
+    image3: 'https://img5.lalafo.com/i/posters/original/6d/4f/91/b7922cc7698c0b9639e0da7f09.jpeg',
+    rating: 5.0,
+    link:'https://www.amazon.com/Xiaomi-Redmi-Note-9S-International/dp/B088LKZHLF/ref=sr_1_1?dchild=1&keywords=xiaomi&qid=1613996000&sr=8-1'
+  },
+  {
+    id: 9,
+    name: 'Xiaomi Redmi 9',
+    price: 409,
+    description: 'Display diagonal, inch: 6.53. Display Resolution: 1080x2340. Operating system: Android 10.0. RAM size: 3. Built-in memory: 32. Number of SIM-cards: 2. Model Year: 2020.',
+    image:'https://alidoss.uz/wp-content/uploads/2020/08/y8mRTQUl106fgf2ijvRBxe9RMMeCOzYZrD3acw52-1.jpeg',
+    image2:'https://eduken.kz/image/cache/catalog/Smartphones/Redmi%209/819bf878-8b9a-4f08-b65d-55472bf88229-720x720.jpg',
+    image3:'https://partnertele.com/images/1000/4894240103487_20200618_RM565-1000.jpg',
+    rating: 4.0,
+    link:'https://www.amazon.com/Xiaomi-Camera-Factory-Unlocked-Smartphone/dp/B08B3NVQ7V/ref=sr_1_10?dchild=1&keywords=xiaomi&qid=1613996189&sr=8-10&th=1'
+  },
+  {
+    id: 10,
+    name: 'Xiaomi Mi 10T',
+    price: 299,
+    description: 'Display diagonal, inch: 6.67. Display Resolution: 1080x2400. Operating system: Android 10.0. RAM size: 6. Built-in memory: 128. Number of SIM-cards: 2. Model Year: 2020.',
+    image:'https://kazandigital.ru/site_shop/files/f_katalog_27/f_dop_izobrajeniya_28/mi_10t_pro_anons_2_10001.jpg',
+    image2:'https://im0-tub-ru.yandex.net/i?id=1e1782a232cb9b4ead0c79ceb52c8a21&ref=rim&n=33&w=300&h=169',
+    image3:'https://im0-tub-ru.yandex.net/i?id=135242726ac3d9e49f2b1038eac4c0f3&ref=rim&n=33&w=338&h=169',
+    rating: 5.0,
+    link:'https://www.amazon.com/Xiaomi-DotDisplay-Battery-Unlocked-Smartphone/dp/B08HJHP6Y6/ref=sr_1_9?dchild=1&keywords=xiaomi&qid=1613997964&sr=8-9'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
