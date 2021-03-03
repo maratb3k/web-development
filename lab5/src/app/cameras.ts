f1d320fa7d93be4ce9b78dd69cd205d3f6955d93
maratb3k <60798717+maratb3k@users.noreply.github.com>
2021-03-03 13:25:11 +0600

    Add files via upload

---
+++ lab5/src/app/cameras.ts
@@ -0,0 +1,63 @@
+export const cameras = [
+  {
+    id: 1,
+    name: 'Digital Camera FHD 1080P Mini Video Camera ',
+    price: 48.99,
+    description: 'Camera for YouTube 2.4 Inch IPS LCD Display Compact Pocket Camera with 16X Digital Zoom Anti-Shake Burst Shoot for Kids Students Teenager',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/61n7iF7cUVL._AC_SL1000_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/61WuoJnCzAL._AC_SL1000_.jpg',
+    image3:'https://images-na.ssl-images-amazon.com/images/I/61CwH4O7ulL._AC_SL1000_.jpg',
+    rating: 5.0,
+    link:'https://www.amazon.com/Digital-YouTube-Anti-Shake-Students-Teenager/dp/B08RYCDLVM/ref=sr_1_2_sspa?dchild=1&keywords=camera&qid=1614513847&sr=8-2-spons&psc=1&smid=A31MNX3EQ17I8R&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExV0pMODJPUk9BTjZCJmVuY3J5cHRlZElkPUEwMjAyMjAwM0ozRUs1M1U3V0c4UiZlbmNyeXB0ZWRBZElkPUEwNTg5NzEzMVVDSFA3Uk5MVFdOMCZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=',
+    like: 125
+    
+  },
+  {
+    id: 2,
+    name: 'Canon PowerShot SX540 Digital Camera',
+    price: 253.99,
+    description: 'Powerful 50x optical zoom with optical image stabilizer for outstanding optical performance. 20.3 Megapixel CMOS sensor with DIGIC 6 Image Processor helps deliver stunning image quality even in low light. Built-in Wi-Fi and NFC allows for easy sharing and transferring of images and videos. Canon High Power Flash HF-DC2. Capture spectacular 1080p Full HD video at 60p with stereo sound',
+    image:'https://images-na.ssl-images-amazon.com/images/I/81l6xdQOHyL._AC_SL1500_.jpg',
+    image2:'https://images-na.ssl-images-amazon.com/images/I/71kmxT8McmL._AC_SL1500_.jpg',
+    image3:'https://images-na.ssl-images-amazon.com/images/I/71ARqfxxp2L._AC_SL1500_.jpg',
+    rating: 4.6,
+    link: 'https://www.amazon.com/Canon-PowerShot-Digital-Camera-Optical/dp/B019UDIAI6/ref=sr_1_3?dchild=1&keywords=camera&qid=1614513847&sr=8-3',
+    like: 357
+  },
+  {
+    id: 3,
+    name: 'Video Camera Camcorder kimire Digital Camera Recorder Full HD 1080P',
+    price: 63.99,
+    description: '1080P(15fps) Video Resolution, 24M(6000x4500) Image Resolution, image format (JPEG), video format(AVI), 16X digital zoom, camcorder with fill light, 3.0 inch LCD and supports 270°rotation, Anti-shake, Face Capture, Beauty Function, Self-timer and Webcam function, Pause function, USB 2.0, TV Output, Setting Date and Time.',
+    image:'https://images-na.ssl-images-amazon.com/images/I/71C6TFutbeL._AC_SL1500_.jpg',
+    image2:'https://images-na.ssl-images-amazon.com/images/I/812jhSp5aCL._AC_SL1500_.jpg',
+    image3:'https://images-na.ssl-images-amazon.com/images/I/819E6T%2BWNrL._AC_SL1500_.jpg',
+    rating: 4.4,
+    link: 'https://www.amazon.com/Camcorder-kimire-Recorder-Rotation-Batteries/dp/B08K7HFMX4/ref=sr_1_6?dchild=1&keywords=camera&qid=1614513847&sr=8-6',
+    like: 344
+  },
+  {
+    id: 4,
+    name: 'Fujifilm Instax Mini 11 Instant Camera - Sky Blue',
+    price: 69,
+    description: 'Brand:	Fujifilm. Film Format Type: Instant. Film Color: NIL. Model Name:	Instax Mini. Exposure Control Type:	Manual. Item Dimensions: LxWxH	2.6 x 4.2 x 4.8 inches. Minimum Shutter Speed:	NIL. Item Weight:	10.4 Ounces. Viewfinder Magnification:	NIL. Maximum Shutter Speed:	1/250 seconds',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/71QB7ylgyBL._AC_SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/71lMSHIzVoL._AC_SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/71y1SQgO1gL._AC_SL1500_.jpg',
+    rating: 4.8,
+    link:'https://www.amazon.com/Fujifilm-Instax-Mini-Instant-Camera/dp/B08527W1MF/ref=sr_1_8?dchild=1&keywords=camera&qid=1614513847&sr=8-8',
+    like: 249
+  },
+  {
+    id: 5,
+    name: 'Fujifilm X100V Digital Camera - Silver',
+    price: 1399,
+    description: 'Movies on the move: The X100V allows you to explore a whole new world of image making with the ability to create stunning 4K video at 30 frames per second. Additionally, super slow-motion footage can be recorded using the camera’s full HD/120p setting, so you can relive details that would otherwise be missed by the naked eye. This professional-level quality is backed up by Fujifilm’s outstanding color reproduction technology and unique selection of in-camera film simulation modes, while those who need more fidelity can record 10-bit, 4:2:2 color via the HDMI port.',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/81WYGGe4mOL._AC_SL1500_.jpg',
+    image2:'https://images-na.ssl-images-amazon.com/images/I/71TFTuh3SgL._AC_SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/71L2JQWsq%2BL._AC_SL1500_.jpg',
+    rating: 4.6,
+    link:'https://www.amazon.com/dp/B084B438ZP/ref=sspa_dk_detail_3?psc=1&pd_rd_i=B084B438ZP&pd_rd_w=8yIPG&pf_rd_p=4269e1a0-a218-4fbd-9748-1cd337d2f2a5&pd_rd_wg=3XaPw&pf_rd_r=9JP50AR9XDFE20GWDS9N&pd_rd_r=cc5a6f8a-3c53-4a0b-acaa-0b02fb9c797c&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExWlNNMkEzMlpHRE1TJmVuY3J5cHRlZElkPUEwNzA1NTMxMkdWTlVXRTNNMDA3WCZlbmNyeXB0ZWRBZElkPUEwOTk5OTUyMUoxUEVUQ0g3UVNQJndpZGdldE5hbWU9c3BfZGV0YWlsJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==',
+    like: 300
+  }
+];
---
+++ lab5/src/app/cart.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { products } from './products';
+import { laptops } from './laptops';
+import { cameras } from './cameras';
+import { tvs } from './tv';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class CartService {
+  items = [];
+  
+
+  constructor(private http: HttpClient) { }
+
+  addToCart(product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
+  }
+  clickToRemove(product){
+    this.items.splice(product, 1);
+  }
+
+}
